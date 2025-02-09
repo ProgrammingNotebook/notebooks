5ccbee8f89ae0b189c4019dd8499fc0763de4987
Gagandeep Singh <gagan.sngh14@gmail.com>
2025-02-09 08:10:40 +0530

    Learning about dictionary

---
+++ python-notebook/basic-syntax-notebook/_025_dictionary.py
@@ -0,0 +1,43 @@
+#-----------------------
+# Creating dictionaries
+#-----------------------
+
+# Creating empty
+
+dic = {}
+
+print(type(dic))
+
+
+# Adding elements to the dictionary
+
+dic['apple'] = 50
+dic['banana'] = 60
+
+print(dic)
+
+
+# Accessing dictionary
+
+print(dic['apple'])
+
+
+# Deleting element from the dictionary
+
+del dic['apple']
+
+print(dic)
+
+dic['pear'] = 30
+dic['apple'] = 90
+dic['kiwi'] = 120
+
+print(dic)
+
+keys_list = dic.keys()
+print(type(keys_list))
+print(keys_list)
+
+value_list = list(dic.values())
+print(type(value_list))
+print(value_list)
